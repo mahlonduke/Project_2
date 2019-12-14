ed2b6d602c3579ef3fbb9c384e94c94753d35fe6
mahlonduke <53539373+mahlonduke@users.noreply.github.com>
2019-12-14 14:17:17 -0800

    First path added

---
+++ api_config.py
@@ -0,0 +1,2 @@
+api_key_atomm = "35fd2afa0b109b9387bbf24e55f321b9"
+api_key_mapbox = "pk.eyJ1IjoibWFobG9uZHVrZSIsImEiOiJjazN2dHB4b2UwZmZ3M2RtZmR5cGY5d3N3In0.q7Tmog7LGaQCv8Ohts6Cdw"
--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import requests
 
-from config import api_key
+import api_config
 
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -18,27 +18,38 @@ def index():
     """Homepage requested."""
     return render_template("index.html")
 
+# Example starter
+@app.route("/names")
+def names():
+    """Return a list of sample names."""
+
+    text = 'some text'
+
+    # Return a list of the column names (sample names)
+    return (text)
 
 @app.route("/data")
-def names():
+def newnames():
     """Data requested"""
     # Create the url
     baseURL = 'https://api.gateway.attomdata.com/propertyapi/v1.0.0/sale/snapshot?pagesize=100&radius=50&latitude='
-    lat = ''
-    lon = ''
+    lat = '30.276930'
+    lon = '-97.743102'
     queryURL = baseURL + lat + '&longitude=' + lon
-
+    #queryURL = 'https://api.gateway.attomdata.com/propertyapi/v1.0.0/sale/snapshot?pagesize=100&radius=50&latitude=30.276930&longitude=-97.743102'
     # Headers for the request
     headers = {
         'Accept': 'application/json',
-        'APIKey': api_key
+        'APIKey': api_config.api_key_atomm
         }
 
     response = requests.get(queryURL, headers=headers)
-    response = response['property']
+    print(response)
+
+    print(queryURL)
 
     # Return the property data in JSON format
-    return jsonify(response)
+    return (queryURL)
 
 
 if __name__ == "__main__":
